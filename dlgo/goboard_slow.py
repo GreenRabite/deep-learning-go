1e22194349ba47b00783660c8d090bcd12233d42
Andy Luo <aluo@greatschools.org>
2019-05-15 15:26:59 -0700

    Initial commit. Added in player class and move class

---
+++ dlgo/goboard_slow.py
@@ -0,0 +1,22 @@
+import copy
+from dlgo.gotypes import Player
+
+class Move():
+  def __init__(self, point=None, is_pass=False, is_resign=False):
+    assert(point is not None) ^ is_pass ^ is_resign
+    self.point = point
+    self.is_play = (self.point is not None)
+    self.is_pass = is_pass
+    self.is_resign = is_resign
+
+  @classmethod
+  def play(cls, point):
+    return Move(point=point)
+
+  @classmethod
+  def pass_turn(cls):
+    return Move(is_pass = True)
+
+  @classmethod
+  def resign(cls):
+    return Move(is_resign = True)
