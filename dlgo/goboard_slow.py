50b1f20ab277f09cf86584ce32c0c3e6bfa76cf9
Andy Luo <aluo@greatschools.org>
2019-05-21 14:32:16 -0700

    add validMove method to GameState class

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -148,3 +148,35 @@ class GameState():
     next_board.place_stone(player, move.point)
     new_string = next_board.get_go_string(move.point)
     return new_string.num_liberties == 0
+  
+  @property
+  def situation(self):
+    return (self.next_player, self.board)
+  
+  def does_move_violate_ko(self, player, move):
+    # returns false if it isnt your turn
+    if not move.is_play:
+      return False
+      # deep copy of the board and plays the move
+    next_board = copy.deepcopy(self.board)
+    next_board.place_stone(player, move.point)
+    next_situation = (player.other, next_board)
+    past_state = self.previous_state
+    while past_state is not None:
+      #checks to see if the past state is exactly the same as the current state
+      if past_state.situation == next_situation:
+        return True
+      past_state = past_state.previous_state
+    return False
+
+    def is_valid_move(self, move):
+      # implements the three board game state checks for Go
+      if self.is_over():
+        return False
+      if move.is_pass or move.is_resign:
+        return True
+      return (
+        self.board.get(move.point) is None and 
+        not self.is_move_self_capture(self.next_player, move) and
+        not self.does_move_violate_ko(self.next_player, move)
+      )
